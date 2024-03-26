26d849656ed73e3f5b607ef9feecfa134325720d
Nick Rabone <n.rabone72@aol.com>
2024-03-26 12:47:17 +0000

    4th - using hx-trigger

--- app.js
+++ app.js
@@ -7,29 +7,36 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>HTMX Essentials</title>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
-        <link rel="icon" href="/icon.png" />
-        <script src="/htmx.js" defer></script>
-        <link rel="stylesheet" href="/main.css" />
-      </head>
-      <body>
-        <header id="main-header">
-          <img src="/htmx-logo.jpg" alt="HTMX Logo" />
-          <h1>Essentials</h1>
-        </header>
+    res.send(`
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>HTMX Essentials</title>
+                <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+                <link rel="icon" href="/icon.png" />
+                <script src="/htmx.js" defer></script>
+                <link rel="stylesheet" href="/main.css" />
+            </head>
+            <body>
+                <header id="main-header">
+                    <img src="/htmx-logo.jpg" alt="HTMX Logo" />
+                    <h1>Essentials</h1>
+                </header>
 
-        <main>
-          <p>HTMX is a JavaScript library that you use without writing JavaScript code.</p>
-          <button hx-get="/info" hx-swap="outerHTML">Learn More</button>
-        </main>
-      </body>
-    </html>
-  `);
+                <main>
+                    <p>HTMX is a JavaScript library that you use without writing JavaScript code.</p>
+                    <button 
+                        hx-get="/info"
+                        hx-trigger="mouseenter[ctrlKey],click" 
+                        hx-target="main" 
+                        hx-swap="beforeend"
+                    >
+                        Learn More
+                    </button>
+                </main>
+            </body>
+        </html>
+    `);
 });
 
 app.get('/info', (req, res) => {
