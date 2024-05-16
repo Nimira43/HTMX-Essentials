139750107d01705ee26426e9094a8232dc0b5f42
Nick Rabone <n.rabone72@aol.com>
2024-05-16 23:18:16 +0100

    Project setup complete with changes made to boilerplate.

---
+++ app.js
@@ -0,0 +1,36 @@
+import express from 'express';
+
+import { F1_KNOWLEDGE } from './data/htmx-info.js';
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Formula 1 Champions</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <link rel="icon" href="/icon.png" />
+        <link rel="stylesheet" href="/main.css" />
+      </head>
+      <body>
+        <header id="main-header">
+          <img src="/mclaren.jpg" alt="mclaren" />
+          <h1>Champions</h1>
+        </header>
+
+        <main>
+          <p>Formula 1 Multiple Championship Winning Drivers.</p>
+          <button>Discover More</button>
+        </main>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(3000);
---
+++ data/htmx-info.js
@@ -0,0 +1,12 @@
+export const F1_KNOWLEDGE = [
+  'Ayrton Senna - 1988, 1990, 1991',
+  'Alain Prost - 1985, 1986, 1989, 1993',
+  'Nelson Piquet - 1981, 1983, 1987',
+  'Niki Lauda - 1975, 1977, 1984',
+  'Michael Schumacher - 1994, 1995, 2000, 2001, 2002, 2003, 2004',
+  'Lewis Hamilton - 2008, 2014, 2015, 2017, 2018, 2019, 2020',
+  'Sebastian Vettel - 2010, 2011, 2012, 2013',
+  'Max Verstappen - 2021, 2022, 2023',
+  'Juan Manuel Fangio - 1951, 1954, 1955, 1956, 1957',
+  'Jackie Stewart - 1969, 1971, 1973',
+];
